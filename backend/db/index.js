86abafa11820ecc8a0f0eeb20c9318254a5b592b
HamiltonTD <thami583@mtroyal.ca>
2020-05-20 18:22:46 -0600

    created backed db index for mongoDB use

---
+++ backend/db/index.js
@@ -0,0 +1,11 @@
+const moongoose = require('mongoose')
+
+mongoose
+    .connect('mongodb//127.0.0.1:27017/trades', { useNewUrlParser: true })
+    .catch(e => {
+	console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
